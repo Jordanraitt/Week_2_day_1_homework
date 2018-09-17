9df83615d61352eba932e6f541d013a424fefb26
jordanraitt <jordanraitt@hotmail.com>
2018-09-17 16:41:13 +0100

    completed + rb file

--- hm_part_B/part_B.rb
+++ hm_part_B/part_B.rb
@@ -1,17 +1,39 @@
 class Team
 
-attr_accessor :team_name, :players, :coach_name
+attr_accessor :team_name, :players, :coach_name, :team_points
 
 def initialize(team_name, players, coach_name)
   @team_name = team_name
   @players = players
   @coach_name = coach_name
+  @team_points = 0
 end
 
 
   def add_players(player)
     @players.push(player)
   end
+
+def check_players_by_name(player_name)
+  for player in @players
+    if player == player_name
+      return player
+    end
+  end
+  return nil
+end
+
+def team_win_lost(result)
+  if result == "win"
+    @team_points += 1
+  elsif result == "loss"
+    return @team_points
+  else
+    return "error"
+  end
+end
+
+
 
 
 
